987811c5a2615b39bacac14a4b3c39266cfcf6c7
Dennie de Lange <621098+tkvw@users.noreply.github.com>
2022-11-11 14:20:24 +0100

    Allow local plugins

--- .devcontainer/rootfs/usr/local/bin/wp-setup.sh
+++ .devcontainer/rootfs/usr/local/bin/wp-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 wait-for-it db:3306
-wait-for-it wordpress:${SITE_PORT}
+wait-for-it wordpress:80
 
 wp core is-installed
 if [ $? -ne 0 ]; then
@@ -10,3 +10,14 @@ if [[ -n "${SITE_PLUGINS}" ]]; then
     wp plugin install $SITE_PLUGINS --activate        
 fi
 wp plugin activate plugin-dev
+
+
+wp_plugins="/var/www/html/wp-content/plugins"
+for p in "$wp_plugins/plugin-dev/.devcontainer/plugins/*"; do
+    destination="/var/www/html/wp-content/plugins/$(basename $p)";
+    if [[ ! -d $destination ]]; then
+        echo "adding local plugin $(basename $p) to wordpress"
+        cp -r $p $destination
+        wp plugin activate $(basename $p)
+    fi    
+done
